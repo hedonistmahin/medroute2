2db10a3225ef9955a7f91403d8990c59ea5abfa8
Samara856 <samaratasnim08@gmail.com>
2023-11-15 22:44:29 +0600

    First commit

--- app/src/main/java/com/example/medroute2/Sineup.java
+++ app/src/main/java/com/example/medroute2/Sineup.java
@@ -7,10 +7,16 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Sineup extends Activity {
 
@@ -18,11 +24,14 @@ public class Sineup extends Activity {
     private TextInputLayout FullName, Email1, Password1, Re_enter_password, Phone;
     private AppCompatButton Go1;
     private Button Login;
+    private FirebaseAuth mAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sineup);
+
+        mAuth = FirebaseAuth.getInstance();
 
         FullName = findViewById(R.id.Full_name);
         Email1 = findViewById(R.id.email);
@@ -47,32 +56,45 @@ public class Sineup extends Activity {
                 final String email = Email1.getEditText().getText().toString().trim();
                 final String password = Password1.getEditText().getText().toString().trim();
                 final String name = FullName.getEditText().getText().toString().trim();
-                final String phone =Phone.getEditText().getText().toString().trim();
+                final String phone = Phone.getEditText().getText().toString().trim();
 
-                if (TextUtils.isEmpty(email)){
+                if (TextUtils.isEmpty(email)) {
 
                     Email1.setError("Email is Required");
                     return;
 
                 }
 
-                if (TextUtils.isEmpty(password)){
+                if (TextUtils.isEmpty(password)) {
 
                     Password1.setError("Password is Required");
                     return;
 
                 }
 
-                if(password.length()<6){
+                if (password.length() < 6) {
 
                     Password1.setError("Must be >= 6 character");
                     return;
 
                 }
 
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Register is Successfull", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Register is Not Successfull", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+                });
             }
+
         });
-
-
     }
 }
+
+
