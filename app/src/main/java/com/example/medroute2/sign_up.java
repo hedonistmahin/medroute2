b7fd469e3e2d7f9bb2558615b30ef79d37751c74
Samara856 <samaratasnim08@gmail.com>
2023-11-16 23:59:10 +0600

    third commit

--- app/src/main/java/com/example/medroute2/sign_up.java
+++ app/src/main/java/com/example/medroute2/sign_up.java
@@ -13,15 +13,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class sign_up extends Activity {
 
     private TextInputLayout Email2, Password2;
     private AppCompatButton Go2;
     private Button sign_up,ForgotPassword;
+    private FirebaseAuth auth;
+    TextView forgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -33,6 +42,8 @@ public class sign_up extends Activity {
         Go2= findViewById(R.id.go_btn2);
         sign_up = findViewById(R.id.signup_btn);
         ForgotPassword = findViewById(R.id.forgotbtn3);
+
+        auth = FirebaseAuth.getInstance();
 
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,26 +59,70 @@ public class sign_up extends Activity {
             public void onClick(View v) {
                 final String email = Email2.getEditText().getText().toString().trim();
                 final String password = Password2.getEditText().getText().toString().trim();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!password.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, password)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(sign_up.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(sign_up.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(sign_up.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        Password2.setError("Empty fields are not allowed");
+                    }
+                } else if (email.isEmpty()) {
+                    Email2.setError("Empty fields are not allowed");
+                } else {
+                    Email2.setError("Please enter correct email");
+                }
             }
         });
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
 
-        ForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(sign_up.this);
-                View dialogView = getLayoutInflater().inflate(R.layout.dialog_forgot, null);
-                EditText emailBox = dialogView.findViewById(R.id.emailBox);
-                builder.setView(dialogView);
-                AlertDialog dialog = builder.create();
+            public void
+onClick(View view) {
+            }
 
-                dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+            {
+        AlertDialog.Builder builder = new AlertDialog.Builder(sign_up.this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_forgot, null);
+
+        EditText emailBox = dialogView.findViewById(R.id.emailBox);
+
+        builder.setView(dialogView);
+        AlertDialog dialog = builder.create();
+
+        dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                String userEmail = emailBox.getText().toString();
+                if (TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+                    Toast.makeText(sign_up.this, "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
-                    public void onClick(View view) {
-                        String userEmail = emailBox.getText().toString();
-                        if (TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
-                            Toast.makeText(sign_up.this, "Enter your registered email id", Toast.LENGTH_SHORT).show();
-                            return;
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(sign_up.this, "Check your email", Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        } else {
+                            Toast.makeText(sign_up.this, "Unable to send, failed", Toast.LENGTH_SHORT).show();
                         }
+                    }
+                });
 
                     }
                 });
@@ -83,8 +138,9 @@ public class sign_up extends Activity {
                 dialog.show();
             }
         });
+        }
     }
-}
+
 
 
 
