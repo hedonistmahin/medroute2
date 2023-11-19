ca68ce984145b7d4d22d275cad2aea8ebaf4b5ca
hedonistmahin <mdmodabbirmahin@gmail.com>
2023-11-19 20:43:51 +0600

    11th Commit

--- app/src/main/java/com/example/medroute2/SearchActivity.java
+++ app/src/main/java/com/example/medroute2/SearchActivity.java
@@ -2,8 +2,12 @@ package com.example.medroute2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,6 +17,13 @@ public class SearchActivity extends Activity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.blackTransparent));
+        }
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setSelectedItemId(R.id.bottom_search);
@@ -35,6 +46,23 @@ public class SearchActivity extends Activity  {
             }
 
             return false;
+        });
+
+
+
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Handle search query submission (e.g., start a search or filter data)
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Handle changes in the search query (e.g., update search results)
+                return true;
+            }
         });
     }
 }
